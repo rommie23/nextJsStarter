92302c01a33c8d0d70b77c453acf73ae499c2309
Rohit Thakur <romi.thakur001@gmail.com>
2024-05-04 17:26:34 +0530

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <h2>Outer Layout Navbar</h2>
+        {children}</body>
     </html>
   );
 }
